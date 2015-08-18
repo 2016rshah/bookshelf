76464e8c49c2a0fff33fa50b553d181609e6368f
Rushi Shah <2016rshah@gmail.com>
2015-08-18 12:12:41 -0400

    Added ability to change ISBN if you want

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -86,7 +86,7 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :user_id)
+      params.require(:book).permit(:title, :author, :user_id, :isbn)
     end
 
     def restrict_access
