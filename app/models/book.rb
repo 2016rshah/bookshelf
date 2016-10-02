d337a08c0addf3c88327c20a045ec74e6f74d0e5
Dima Nazarchuk <dmitry.nazarchuk@kandasoft.com>
2016-10-03 00:56:55 +0300

    Add uniqueness constraint for book's isbn and refactor little bit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
 	belongs_to :user
-	validates_presence_of :title, :author, :user_id, :isbn
+	validates_presence_of :title, :author, :user_id
+  validates :isbn, uniqueness: true
 end
